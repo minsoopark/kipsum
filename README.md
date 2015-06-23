78b8d633651e7a3e4432b461f564f3ba54d01cf3
Minsoo Park <minsoo1003@gmail.com>
2015-06-23 14:34:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Kipsum
+
+[Lorem Ipsum](https://ko.wikipedia.org/wiki/로렘_입숨)과 같은 무의미한 텍스트를 한글로 생성해줍니다.
+
+[한글입숨](http://hangul.thefron.me/)을 이용하였습니다.
---
+++ kipsum.go
@@ -0,0 +1,61 @@
+package kipsum
+
+import (
+    "net/http"
+    "fmt"
+    "os"
+    "strconv"
+    "io/ioutil"
+    "encoding/json"
+)
+
+var lengths = []string{"long", "medium", "short"}
+
+func main() {
+    paragraphs := 3
+    length := "medium"
+    for i, arg := range os.Args[1:] {
+        if i == 0 {
+            if v, err := strconv.Atoi(arg); err == nil {
+                paragraphs = v
+            }
+        } else if i == 1 {
+            if contains(lengths, arg) {
+                length = arg
+            }
+        }
+    }
+    url := fmt.Sprintf("http://hangul.thefron.me/generate.json?paragraphs=%d&length=%s", paragraphs, length)
+    res, err := http.Get(url)
+
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    body := res.Body
+    defer body.Close()
+
+    contents, err_ := ioutil.ReadAll(body)
+
+    if err_ != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println(read(string(contents)))
+}
+
+func contains(strs []string, comp string) bool {
+    for _, str := range strs { if str == comp { return true } }
+    return false
+}
+
+func read(contents string) string {
+    var data map[string]interface{}
+    byt := []byte(contents)
+    if err := json.Unmarshal(byt, &data); err != nil {
+        return "Couldn't read from json!"
+    }
+    return data["ipsum"].(string)
+}
